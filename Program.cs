78f78aaf60b870c14fcaab4824e0fae39a13a410
marsimjob <mario.jobb@gmail.com>
2025-11-17 10:42:40 +0100

    Latest

--- Program.cs
+++ Program.cs
@@ -14,7 +14,59 @@ namespace DataVerseManager
     {
         static void Main(string[] args)
         {
-          
+            //AccountManager.RegisteredUsers = JsonHandeler.LoadJson("registeredUsers.json");
+            //Console.OutputEncoding = Encoding.UTF8;
+            //AccountManager.LoadLogInMenu();
+            Console.InputEncoding = System.Text.Encoding.UTF8;
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            BettingFunctionTest();
+      
+
+        }
+
+        static void BettingFunctionTest()
+        {
+            Team Lakers = new Team();
+            Team Bulls = new Team();
+            Team Heat = new Team();
+            Team Warriors = new Team();
+            Team Knicks = new Team();
+
+            Heat.TeamName = "Heat";
+            Lakers.TeamName = "Lakers";
+            Bulls.TeamName = "Bulls";
+            Knicks.TeamName = "Knicks";
+            Warriors.TeamName = "Warriors";
+
+            Console.WriteLine(Lakers.WinRate);
+            Console.ReadLine();
+
+            Team.BuildTeam(Lakers);
+            Lakers.CalculateWinLossRate();
+            Console.WriteLine(Lakers.WinRate);
+            Console.ReadLine();
+
+            Heat.WinRate = 73;
+            Knicks.WinRate = 90;
+            Warriors.WinRate = 52;
+            Bulls.WinRate = 31;
+
+            // Put this in a wallet and make it track with json etc
+            // User --- Wallet as attribute?
+            double bettingCash = 10000;
+
+            Coach myCoach = new Coach();
+            Coach yourCoach = new Coach();
+
+            myCoach.UserWallet.GetMoney(bettingCash);
+            myCoach.CoachName = "Nemo";
+            myCoach.CoachTeam.WinRate = 67;
+            myCoach.CoachTeam.TeamName = ".NETters";
+
+            yourCoach.UserWallet.GetMoney(bettingCash);
+            yourCoach.CoachName = "Bavel";
+            yourCoach.CoachTeam.WinRate = 27;
+            yourCoach.CoachTeam.TeamName = "Crystals";
 
             // LOW RANKERS
             Match.RunVisualMatch(Bulls, yourCoach.CoachTeam);
